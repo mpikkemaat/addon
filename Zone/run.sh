44177dc8eea4bcad5ae6708c51ec1b246f049fd3
marnix <marnix.pikkemaat@gmail.com>
2020-04-25 23:27:04 +0200

    small changes

--- Zone/run.sh
+++ Zone/run.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/with-contenv bashio
 
-echo Hello world!
+CONFIG_PATH=/data/options.json
+
+echo "This system appears to be a $BUILD_ARCH"
+
+SERIALPORT="$(jq --raw-output '.serialport' $CONFIG_PATH)"
+/tunslip6  -s $SERIALPORT fd00::1/64
 
