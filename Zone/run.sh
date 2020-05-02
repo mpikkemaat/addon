803fecd84830b6aed406d0972a57ea23ae35d688
marnix <marnix.pikkemaat@gmail.com>
2020-05-02 20:03:31 +0200

    removed test line from run.sh

--- Zone/run.sh
+++ Zone/run.sh
@@ -3,10 +3,8 @@
 CONFIG_PATH=/data/options.json
 
 SERIALPORT="$(jq --raw-output '.serialport' $CONFIG_PATH)"
-#BUILD_ARCH="$(jq --raw-output ".build_arch" $CONFIG_PATH)"
 
 echo "Zone addon starting"
-echo "This system appears to be a $BUILD_ARCH"
 
 /tunslip6  -s $SERIALPORT fd00::1/64
 
