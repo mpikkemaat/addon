c556ab601337dbd381af4d9048f28dfee7010eb8
marnix <marnix.pikkemaat@gmail.com>
2020-05-02 17:32:46 +0200

    Added armhf target

--- Zone/run.sh
+++ Zone/run.sh
@@ -2,8 +2,11 @@
 
 CONFIG_PATH=/data/options.json
 
+SERIALPORT="$(jq --raw-output '.serialport' $CONFIG_PATH)"
+BUILD_ARCH=$(jq --raw-output ".build_arch" $CONFIG_PATH)
+
+echo "Zone addon starting"
 echo "This system appears to be a $BUILD_ARCH"
 
-SERIALPORT="$(jq --raw-output '.serialport' $CONFIG_PATH)"
 /tunslip6  -s $SERIALPORT fd00::1/64
 
