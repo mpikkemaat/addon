064d2c7bb08161fead1aabbc1a750460c2374f1c
marnix <marnix.pikkemaat@gmail.com>
2020-05-02 19:00:13 +0200

    Changes to the config files

--- Zone/run.sh
+++ Zone/run.sh
@@ -3,7 +3,7 @@
 CONFIG_PATH=/data/options.json
 
 SERIALPORT="$(jq --raw-output '.serialport' $CONFIG_PATH)"
-BUILD_ARCH=$(jq --raw-output ".build_arch" $CONFIG_PATH)
+BUILD_ARCH="$(jq --raw-output ".build_arch" $CONFIG_PATH)"
 
 echo "Zone addon starting"
 echo "This system appears to be a $BUILD_ARCH"
