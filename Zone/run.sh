207adddcc576dd2a8df55f79cc10170578304a20
marnix <marnix.pikkemaat@gmail.com>
2020-05-06 21:58:00 +0200

    more testing

--- Zone/run.sh
+++ Zone/run.sh
@@ -1,16 +1,9 @@
-#!/usr/bin/with-contenv bashio
 
 CONFIG_PATH=/data/options.json
 
 SERIALPORT="$(jq --raw-output '.serialport' $CONFIG_PATH)"
 
-echo "Zone addon starting"
-
-ls -la
-
 echo "serial port is set to $SERIALPORT"
 
-ldd /tunslip6
-
-/tunslip6  -s $SERIALPORT fd00::1/64
+/tunslip6 -s $SERIALPORT fd00::1/64
 
