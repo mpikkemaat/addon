b4992d40f0bf93d3146ffec2d9fc91d7ebdb57cb
marnix <marnix.pikkemaat@etna-ct.com>
2020-12-02 13:22:46 +0100

    more debugging info

--- Ion/run.sh
+++ Ion/run.sh
@@ -4,8 +4,7 @@ CONFIG_PATH=/data/options.json
 SERIALPORT="$(jq --raw-output '.serialport' $CONFIG_PATH)"
 
 echo "serial port is set to $SERIALPORT"
-host
-hostname
-hostname -i
+host $(hostname)
+ifconfig
 /tunslip6 -s $SERIALPORT -v4 fd00::1/64
 
