f3ada9db9c15b3f5c44d0b57079e69bc3a3d20c1
marnix <marnix.pikkemaat@etna-ct.com>
2020-12-02 11:19:59 +0100

    Removed ip address, doesn't work

--- Ion/run.sh
+++ Ion/run.sh
@@ -5,5 +5,5 @@ SERIALPORT="$(jq --raw-output '.serialport' $CONFIG_PATH)"
 
 echo "serial port is set to $SERIALPORT"
 
-/tunslip6 -s $SERIALPORT -a 172.30.32.1 -v4 fd00::1/64
+/tunslip6 -s $SERIALPORT -v4 fd00::1/64
 
