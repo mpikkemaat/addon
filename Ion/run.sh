8301b0e6db9d477661bc86e43c46622039da15a6
mpikkemaat <marnix.pikkemaat@gmail.com>
2021-12-29 12:55:35 +0100

    Update run.sh
    Changed tunslip verbosity to prevent crash

--- Ion/run.sh
+++ Ion/run.sh
@@ -11,6 +11,4 @@ echo "Looking up hostname:"
 host $(hostname)
 hostname -i
 
-/tunslip6 -v -s $SERIALPORT fd00::1/64
-#try to prevent some packets forcing the process to exit
-echo 0 > /proc/sys/net/ipv6/auto_flowlabels
+/tunslip6 -v1 -s $SERIALPORT fd00::1/64
